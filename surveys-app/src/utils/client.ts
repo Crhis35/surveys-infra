2b607c6d58bd70ff4c9a1f3e34fb19f92eb82443
Crhistian Caraballo <cristianjcj35@gmail.com>
2023-07-27 09:04:14 -0500

    Added endpoint

--- surveys-app/src/utils/client.ts
+++ surveys-app/src/utils/client.ts
@@ -1,3 +1,6 @@
 import { GraphQLClient } from 'graphql-request';
 
-export const gqlClient = new GraphQLClient(import.meta.env.VITE_GRAPHQL_URL);
+export const gqlClient = new GraphQLClient(
+  import.meta.env.VITE_GRAPHQL_URL ||
+    'https://survey546.azurewebsites.net/graphql'
+);
